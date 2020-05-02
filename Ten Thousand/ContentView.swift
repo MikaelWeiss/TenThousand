eb3cbd88b62efb6643e402c609e692e7f6f24147
Mikael Weiss <Campingmikael@icloud.com>
2020-05-01 18:10:09 -0600

    Added the button different. Got rid of a file.

--- Ten Thousand/ContentView.swift
+++ Ten Thousand/ContentView.swift
@@ -28,6 +28,8 @@ struct ContentView: View {
                 }
                 StopwatchImage()
                 Spacer()
+                ActionButton(title: "Get Started", hight: 54, width: 335, fontSize: 30)
+                Spacer()
             }
             .font(.custom("AvenirNext-DemiBold", size: 20))
             .padding(.top, 45)
@@ -54,5 +56,35 @@ struct StopwatchImage: View {
                 .opacity(0.4)
             
         }.shadow(radius: 10, x: 10, y: 10)
+    }
+}
+
+
+struct ActionButton: View {
+    var buttonName: String
+    var buttonHight: CGFloat
+    var buttonWidth: CGFloat
+    var fontSize: CGFloat
+    
+    var body: some View {
+        Button(action: {
+            
+        }) {
+            Text(buttonName)
+                .font(.custom("Avenir-Black", size: fontSize))
+                .foregroundColor(Color.white)
+                .opacity(0.85)
+                .frame(width: 335.0, height: 54.0)
+                .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.1960784314, green: 0.7725490196, blue: 1, alpha: 1))]), startPoint: .bottomLeading, endPoint: .topTrailing))
+                .cornerRadius(27)
+                .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)), radius: 5, x: 0, y: 4)
+        }
+    }
+    
+    init(title: String, hight: CGFloat, width: CGFloat, fontSize: CGFloat) {
+        buttonName = title
+        buttonHight = hight
+        buttonWidth = width
+        self.fontSize = fontSize
     }
 }
