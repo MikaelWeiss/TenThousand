fc6bacc2a34dd5ae1ef623b605f011986bc52955
Mikael Weiss <Campingmikael@icloud.com>
2020-05-04 16:36:31 -0600

    Added the necessary files for the project based off of my new design. I might add one more file for the log, but I'm not sure about that yet.

---
+++ Ten Thousand/TimerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerView.swift
+//  Ten Thousand
+//
+//  Created by Mikael Weiss on 5/4/20.
+//  Copyright © 2020 Mikael Weiss. All rights reserved.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
---
+++ Ten Thousand/UpdateLogListView.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateLogListView.swift
+//  Ten Thousand
+//
+//  Created by Mikael Weiss on 5/4/20.
+//  Copyright © 2020 Mikael Weiss. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateLogListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateLogListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateLogListView()
+    }
+}
