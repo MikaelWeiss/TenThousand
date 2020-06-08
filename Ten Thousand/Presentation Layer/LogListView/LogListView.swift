65cb6236cd155ca95216f2917f228a051ab92c75
Mikael Weiss <Campingmikael@icloud.com>
2020-06-08 15:23:54 -0600

    Added a file

---
+++ Ten Thousand/Presentation Layer/LogListView/LogListView.swift
@@ -0,0 +1,21 @@
+//
+//  LogListView.swift
+//  Ten Thousand
+//
+//  Created by Mikael Weiss on 6/8/20.
+//  Copyright © 2020 Mikael Weiss. All rights reserved.
+//
+
+import SwiftUI
+
+struct LogListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogListView()
+    }
+}
