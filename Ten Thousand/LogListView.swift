43c913177180f4bbb645b7fd7a004ae782929856
Mikael Weiss <Campingmikael@icloud.com>
2020-06-08 12:50:45 -0600

    Commit before I branch.

--- Ten Thousand/LogListView.swift
+++ Ten Thousand/LogListView.swift
@@ -14,6 +14,7 @@ import SwiftUI
 struct LogListView: View {
     @EnvironmentObject var userSettings: UserSettings
     @ObservedObject var listOfUserLogs = ListOfUserLogs()
+    @State private var showChoseLogList = false
     
     var body: some View {
         VStack {
@@ -24,7 +25,7 @@ struct LogListView: View {
                     .environmentObject(userSettings)
                 
                 Button(action: {
-                    
+                    self.showChoseLogList.toggle()
                 }) {
                     Text(listOfUserLogs.allLogLists[listOfUserLogs.currentLog].name)
                         .font(.system(.largeTitle, design: .rounded)).bold()
--- Ten Thousand/UpdateLogListView.swift
+++ Ten Thousand/UpdateLogListView.swift
@@ -9,9 +9,60 @@
 import SwiftUI
 
 struct UpdateLogListView: View {
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    @State private var selectedMonthIndex = Calendar.current.component(.month, from: Date()) - 1
+    @State private var selectedDayIndex = Calendar.current.component(.day, from: Date()) - 1
+    @State private var selectedYearIndex = Calendar.current.component(.year, from: Date()) - 3
+    @State private var fakeSelection = 0
     
     var body: some View {
-        DurationPickerView()
+        HStack(spacing: 0) {
+            
+            LableForInPicker(lable: "Month", width: 80)
+            
+            Picker("Month", selection: $selectedMonthIndex) {
+                ForEach (0..<months.count) {
+                    Text(self.months[$0])
+                }
+            }
+            .labelsHidden()
+            .frame(width: 40)
+            .clipped()
+            
+            LableForInPicker(lable: " Day", width: 45)
+            
+            Picker("Month", selection: $selectedDayIndex) {
+                ForEach (findDaysInMonth(selectedMonthIndex)) {
+                    Text("\($0)")
+                }
+            }
+            .labelsHidden()
+            .frame(width: 40)
+            .clipped()
+            
+            LableForInPicker(lable: "Year", width: 40)
+            
+            Picker("Month", selection: $selectedYearIndex) {
+                ForEach (2010..<Calendar.current.component(.year, from: Date()) + 1) {
+                    Text("\($0)")
+                }
+            }
+            .labelsHidden()
+            .frame(width: 80)
+            .clipped()
+            
+        }
+    }
+}
+
+func findDaysInMonth(_ index: Int) -> Range<Int> {
+    switch index {
+    case 3,5,8,10:
+        return 1..<31
+    case 1:
+        return 1..<30
+    default:
+        return 1..<32
     }
 }
 
@@ -23,3 +74,24 @@ struct UpdateLogListView_Previews: PreviewProvider {
 
 
 
+
+struct LableForInPicker: View {
+    let lable: String
+    let width: Int
+    
+    var body: some View {
+        ZStack {
+            Picker("", selection: .constant(0)) {
+                Text(" ")
+                    .bold()
+            }
+            .labelsHidden()
+            .frame(width: CGFloat(width))
+            .clipped()
+            
+            Text("\(lable)")
+                .font(.system(size: 19, weight: .bold, design: .rounded))
+        }
+        .accessibility(hidden: true)
+    }
+}
