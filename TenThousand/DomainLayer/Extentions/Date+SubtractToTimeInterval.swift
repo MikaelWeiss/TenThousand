1ef906720a3fb92135520faf97f9c89b266bc552
MikaelWeiss <campingmikael@icloud.com>
2021-09-29 17:14:07 -0600

    I don't know what changed

--- TenThousand/DomainLayer/Extentions/Date+SubtractToTimeInterval.swift
+++ TenThousand/DomainLayer/Extentions/Date+SubtractToTimeInterval.swift
@@ -10,6 +10,6 @@ import Foundation
 
 extension Date {
     static func - (lhs: Date, rhs: Date) -> TimeInterval {
-        return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
+        return lhs.timeIntervalSince1970 - rhs.timeIntervalSince1970
     }
 }
